309ba15ab49c52c5b56996ee2059c05790654a6f
Finn Oesterreicher <finn.oesterreicher@gmx.de>
2024-11-08 17:03:14 +0100

    Aufgabe 2 completed

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/HamsterController.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/HamsterController.java
@@ -9,7 +9,8 @@ import de.hamstersimulator.objectsfirst.external.model.Hamster;
  * A HamsterController knows some behavioural patterns for hamsters and makes
  * the controlled hamsters act according to those patterns.
  *
- * @author (Your name)
+ * @author Finn Österreicher 3777877 st193897@stud.uni-stuttgart.de
+ * @author Tim Zimmermann 3787155 st192571@stud.uni-stuttgart.de
  *
  */
 public class HamsterController {
@@ -58,13 +59,39 @@ public class HamsterController {
 	// TODO replace this comment with JavaDoc, including Pre and Post Conditions.
 
 	/**
-	 * Lets the hamster do a left half circle
+	 * Lets the hamster do a left half circle.
+	 * Can be used for setting the hamster up to go on one row bellow when he has reached a left wall.
+	 * Pre: There is a field to the hamsters left
+	 * Post: Hamster is one field to the left where he started and turned in the opposite direction
 	 */
-	void hamsterHalfCircle() {
-		controlledHamster.write("Executing helper0:");
+	void hamsterLeftHalfCircle() {
+		controlledHamster.write("Executing hamsterLeftHalfCircle:");
 		controlledHamster.turnLeft();
 		controlledHamster.move();
 		controlledHamster.turnLeft();
+		controlledHamster.write("I ran a half circle!");
+	}
+
+	/**
+	 * turns the hamster right by turning him left three times
+	 */
+	void hamsterTurnRight(){
+		for(int i=0; i<3; i++) {
+			controlledHamster.turnLeft();
+		}
+	}
+
+	/**
+	 * Lets the hamster do a right half circle.
+	 * Can be used for setting the hamster up to go on one row bellow when he has reached a right wall.
+	 * Pre: There is a field to the hamsters right
+	 * Post: Hamster is one field to the right where he started and turned in the opposite direction
+	 */
+	void hamsterRightHalfCircle() {
+		controlledHamster.write("Executing hamsterRightHalfCircle:");
+		hamsterTurnRight();
+		controlledHamster.move();
+		hamsterTurnRight();
 		controlledHamster.write("I ran a half circle!");
 	}
 
@@ -72,12 +99,15 @@ public class HamsterController {
 
 	/**
 	 * Lets the hamster pick up all grains and move forward while clear
+	 * Pre: There are none besides that the hamster has to exist
+	 * Post: Hamster is infront of a wall and he picked up all grains that he has passed
 	 */
 	void hamsterMoveAndPick() {
-		controlledHamster.write("Executing helper1:");
+		controlledHamster.write("Executing hamsterMoveAndPick:");
+		pickAllGrains();	// Modified for better use
 		while (controlledHamster.frontIsClear()) {
-			pickAllGrains();
 			controlledHamster.move();
+			pickAllGrains();	// Modified for better use
 		}
 		controlledHamster.write("I picked all grains, on the tile behind me!");
 	}
@@ -87,9 +117,11 @@ public class HamsterController {
 	/**
 	 * Turns the hamster left turns times
 	 * @param turns Integer count of turns the hamster should make
+	 * Pre: There are none besides that the hamster has to exist
+	 *Post: Hamster turned turns times and looks towards the corresponding direction. If turns mod 4 = 0 -> same direction
 	 */
 	void hamsterTurn(Integer turns) {
-		controlledHamster.write("Executing helper2:");
+		controlledHamster.write("Executing hamsterTurn:");
 		for (Integer i = 0; i < turns; i++) {
 			controlledHamster.turnLeft();
 		}
@@ -98,9 +130,12 @@ public class HamsterController {
 
 	// TODO replace this comment with JavaDoc, including Pre and Post Conditions.
 
+
 	/**
 	 * Moves the hamster either steps times or until the wall is reached
 	 * @param steps Integer count of steps the hamster should make
+	 * Pre: There are none besides that the hamster has to exist
+	 * Post: Hamster either moved steps times or is infront of a wall
 	 */
 	void hamsterMoveSteps(Integer steps) {
 		controlledHamster.write("Executing helper3:");
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/TwoHamstersHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/homework/TwoHamstersHamsterGame.java
@@ -3,17 +3,57 @@ package de.unistuttgart.iste.sqa.pse.sheet03.homework;
 /**
  * Describe the class TwoHamstersHamsterGame here.
  *
- * @author (Your name)
+ * @author Finn Österreicher 3777877 st193897@stud.uni-stuttgart.de
+ * @author Tim Zimmermann 3787155 st192571@stud.uni-stuttgart.de
  *
  */
 public class TwoHamstersHamsterGame extends InternalTwoHamstersHamsterGame {
 
 	@Override
 	void hamsterRun() {
-		// delete the following lines, once you figured out what the helpers do.
-		controllerTiffany.pickAllGrains();
-		controllerPaule.putAllGrains();
 
 		// TODO implement Exercise 2 (b) here
+		// Paule pick all grains and move to the meeting point
+		// Row 1
+		controllerPaule.hamsterMoveAndPick();
+		controllerPaule.hamsterRightHalfCircle();
+
+		// Row 2
+		controllerPaule.hamsterMoveAndPick();
+		controllerPaule.hamsterLeftHalfCircle();
+
+		// Row 3
+		controllerPaule.hamsterMoveAndPick();
+		controllerPaule.hamsterRightHalfCircle();
+
+		// Row 4
+		controllerPaule.hamsterMoveAndPick();
+		controllerPaule.hamsterTurn(2);
+		controllerPaule.hamsterMoveSteps(3);
+
+
+		//Tiffany pick all grains and move to the meeting point
+		// Set up tiffany towards the right direction
+		controllerTiffany.hamsterTurn(1);
+		// Row 1
+		controllerTiffany.hamsterMoveAndPick();
+		controllerTiffany.hamsterRightHalfCircle();
+
+		// Row 2
+		controllerTiffany.hamsterMoveAndPick();
+		controllerTiffany.hamsterLeftHalfCircle();
+
+		// Row 3
+		controllerTiffany.hamsterMoveAndPick();
+		controllerTiffany.hamsterRightHalfCircle();
+		controllerTiffany.pickAllGrains();
+		// Row 4
+		controllerTiffany.hamsterMoveSteps(3);
+
+
+		// Put all grains
+		controllerTiffany.putAllGrains();
+		controllerPaule.putAllGrains();
+
 	}
 }
